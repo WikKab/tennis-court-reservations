385bcefa1e1038f0ea836c8286129531e2acbf35
Wiktor <wiktor5076@o2.pl>
2022-07-07 09:35:48 +0200

    Exact detail reservation

---
+++ reservations/tests/test_views.py
@@ -0,0 +1,36 @@
+from django.test import RequestFactory, TestCase
+
+from reservations.models import TennisCourt
+from reservations.views import CreateExactCourtReservationFormView
+
+class TestViews(TestCase):
+
+    def test_cost_without_rent(self):
+        request = RequestFactory().get('/')
+        view = CreateExactCourtReservationFormView()
+        view.setup(request)
+        self.court = TennisCourt(hire_price=100, equipment_rent=False)
+        reservation_start = '16:00'
+        reservation_end = '18:00'
+
+        expected = 400
+
+        result = view._calculate_cost(reservation_start, reservation_end, self.court, False)
+
+        self.assertEquals(result, expected)
+
+    def test_cost_with_rent(self):
+        request = RequestFactory().get('/')
+        view = CreateExactCourtReservationFormView()
+        view.setup(request)
+        self.court = TennisCourt(hire_price=100, equipment_cost=20, equipment_rent=True)
+        reservation_start = '16:00'
+        reservation_end = '18:00'
+
+        expected = 420
+
+        result = view._calculate_cost(reservation_start, reservation_end, self.court, True)
+
+        self.assertEquals(result, expected)
+
+
